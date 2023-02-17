770968783c0890ceb176eae8cf8a1d9fb8135297
sumon <sumonmondal1530765@gmail.com>
2023-02-17 19:38:31 +0600

    database connected successfully

--- server/database/db.js
+++ server/database/db.js
@@ -2,14 +2,14 @@ import mongoose from "mongoose"
 
 
 
-const connection =async()=>{
-    const URL = `mongodb://user:12345@ac-mnstkxt-shard-00-00.4x6yrnh.mongodb.net:27017,ac-mnstkxt-shard-00-01.4x6yrnh.mongodb.net:27017,ac-mnstkxt-shard-00-02.4x6yrnh.mongodb.net:27017/?ssl=true&replicaSet=atlas-v1nqjy-shard-0&authSource=admin&retryWrites=true&w=majority`;
+const connection =async(username,password)=>{
+    const URL = `mongodb://user:12345@ac-mmmrmzu-shard-00-00.fzzajua.mongodb.net:27017,ac-mmmrmzu-shard-00-01.fzzajua.mongodb.net:27017,ac-mmmrmzu-shard-00-02.fzzajua.mongodb.net:27017/?ssl=true&replicaSet=atlas-ag09d0-shard-0&authSource=admin&retryWrites=true&w=majority`;
     
     mongoose.set('strictQuery',false)
 
     try{
       await  mongoose.connect(URL, { useNewUrlParser : false });
-      console.log('Databse connected successfully')
+      console.log('Database connected successfully')
     }catch(error){
         console.log('error while connecting with database', error );
     }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import connection from "./database/db.js";
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express()
 
 const PORT = 8000;
 
 app.listen(PORT , () => console.log(`server is running sucessfully on PORT Welcome to the port ${PORT}`))
-connection();
+
+const USERNAME =process.env.DB_USERNAME;
+const PASSWORD =process.env.DB_PASSWORD;
+
+connection(USERNAME , PASSWORD);
